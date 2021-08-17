cc61867addba9605192bb447bfdb61ca02eaf001
admond <admondtamang@gmail.com>
2021-08-17 23:24:16 +0545

    first commit

---
+++ app/pages/LandingPage/index.tsx
@@ -0,0 +1,57 @@
+import React, { Suspense } from "react"
+
+import { Image, Link, BlitzPage, useMutation, Routes } from "blitz"
+import Layout from "app/core/layouts/Layout"
+import { useCurrentUser } from "app/core/hooks/useCurrentUser"
+import logout from "app/auth/mutations/logout"
+const UserInfo = () => {
+  const currentUser = useCurrentUser()
+  const [logoutMutation] = useMutation(logout)
+
+  if (currentUser) {
+    return (
+      <>
+        <button
+          className="button small"
+          onClick={async () => {
+            await logoutMutation()
+          }}
+        >
+          Logout
+        </button>
+        <div>
+          User id: <code>{currentUser.id}</code>
+          <br />
+          User role: <code>{currentUser.role}</code>
+        </div>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <Link href={Routes.SignupPage()}>
+          <a className="button small">
+            <strong>Sign Up</strong>
+          </a>
+        </Link>
+        <Link href={Routes.LoginPage()}>
+          <a className="button small">
+            <strong>Login</strong>
+          </a>
+        </Link>
+      </>
+    )
+  }
+}
+
+const LandingPage: BlitzPage = () => {
+  return (
+    <div>
+      <Suspense fallback="Loading...">
+        <UserInfo />
+      </Suspense>
+      <p>LandingPage</p>
+    </div>
+  )
+}
+export default LandingPage
